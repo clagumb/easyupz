1544c1ac580b2315b7be7fc38215a3eb0c5e6270
Claus Gumbmann <cg.gn@t-online.de>
2025-06-08 14:53:43 +0200

    Anpassungen Navigation für Screenshot

--- frontend/src/components/aside/aside.tsx
+++ frontend/src/components/aside/aside.tsx
@@ -3,7 +3,19 @@ import "./aside.css";
 export default function Aside() {
     return (
         <aside style={{ gridArea: "sidebar" }}>
-            Nav
+            <p>Unterrichtspflichtszeit</p>
+            <a href="#">Gesamtansicht</a><br />
+            <a href="#">Einzelbetrachtung</a><br />
+            <a href="#">Anrechnungen</a><br />
+            <a href="#">Ermäßigungen</a><br />
+
+            <p>Administration</p>
+            <a href="#">Lehrkräfte</a><br />
+            <a href="#">Schuljahre</a><br />
+            <a href="#">Anrechnungen</a><br />
+            <a href="#">Ermäßigungen</a><br />
+            <a href="#">Schulen</a><br />
+            <a href="#">Benutzerverwaltung</a>
         </aside>
     );
 }
